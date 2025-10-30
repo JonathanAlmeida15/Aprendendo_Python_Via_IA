fe90b1489c2006be4e1155dad5618d3b92b043a8
JonathanAlmeida15 <jonathangabrielcar@gmail.com>
2025-10-30 00:00:04 -0300

    Subindo o projeto de regressão linear de preços de casas da california, funciona!!

--- regressao_linear_precos_casas.py
+++ regressao_linear_precos_casas.py
@@ -1,4 +1,7 @@
 from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
 import pandas as pd
 
 # Carregar dataset
@@ -7,3 +10,16 @@ tabela = dados.frame
 
 print(tabela.head())
 print(tabela.describe())
+
+X = tabela.drop(columns=['MedHouseVal'])  # Entradas
+y = tabela['MedHouseVal']                 # Saída
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+modelo = LinearRegression()
+modelo.fit(X_train, y_train)
+
+y_pred = modelo.predict(X_test)
+
+print("MSE (Erro Quadrático Médio):", mean_squared_error(y_test, y_pred))
+print("R² (Coeficiente de Determinação):", r2_score(y_test, y_pred))
